938d6ee8c26409e957444be5d7d74de4325a138b
EdwardAIE <edward.bellingham@gmail.com>
2020-04-25 14:00:44 +1000

    Try 3

--- reference.js
+++ reference.js
@@ -1,9 +1,20 @@
 			document.getElementById("Everything").style.zoom = window.innerWidth * 0.001;
 			
 			var fs = require('browserify-fs');
-			var file = fs.readFileSync("read.txt", "utf8");
-			console.log(file);
-			
+	fs.readFile('./read.txt', function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    const content = data;
+
+    // Invoke the next step here however you like
+    console.log(content);   // Put all of the code here (not the best solution)
+    processFile(content);   // Or put the next step in a function and invoke it
+});
+
+function processFile(content) {
+    console.log(content);
+}
 			
 			function RandomFunction()
 			{
